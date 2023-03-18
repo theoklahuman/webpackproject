c1cdae9bd6d6caadd89b45a541240b0a2de6ea88
theoklahuman <theoklahuman@gmail.com>
2023-03-18 17:56:18 -0500

    Work on footer

--- src/homepage.js
+++ src/homepage.js
@@ -1,3 +1,9 @@
+import "./style.css";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import twitter from "./twitter.svg";
+import logo from "./mcd.jpg";
+
 export default class Newpage {
   constructor(name) {
     this.name = name;
@@ -15,6 +21,18 @@ export default class Newpage {
     headerDiv.insertAdjacentElement("afterbegin", headerMain);
     headerDiv.insertAdjacentElement("beforeend", headerMainAboutInfo);
     header.appendChild(headerDiv);
+    const mainLogo = new Image();
+    mainLogo.id = "main-logo";
+    mainLogo.src = logo;
+    mainLogo.alt = "business logo";
+    header.insertAdjacentElement("afterbegin", mainLogo);
+    const menuButton = document.createElement("div");
+    menuButton.className = "menu-button";
+    for (let i = 0; i < 3; i++) {
+      const hamburgerLine = document.createElement("span");
+      menuButton.insertAdjacentElement("beforeend", hamburgerLine);
+    }
+    header.insertAdjacentElement("beforeend", menuButton);
     return header;
   }
 
@@ -74,10 +92,46 @@ export default class Newpage {
   }
 
   makeFooter() {
-    const section = document.createElement("section");
-    const paragraph = document.createElement("p");
-    paragraph.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at tellus at urna condimentum mattis. Ultrices vitae auctor eu augue. Vulputate eu scelerisque felis imperdiet proin fermentum. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Et magnis dis parturient montes nascetur ridiculus mus mauris.`;
-    section.appendChild(paragraph);
-    return section;
+    const facebookLogo = new Image();
+    facebookLogo.src = facebook;
+    facebookLogo.alt = "facebook logo";
+    facebookLogo.className = "footer-logos";
+    const instagramLogo = new Image();
+    instagramLogo.src = instagram;
+    instagramLogo.alt = "instagram logo";
+    instagramLogo.className = "footer-logos";
+    const twitterLogo = new Image();
+    twitterLogo.src = twitter;
+    twitterLogo.alt = "twitter logo";
+    twitterLogo.className = "footer-logos";
+    const homepageFooter = document.createElement("footer");
+    const homepageFooterParagraph = document.createElement("p");
+    homepageFooterParagraph.className = "footer-main-message";
+    homepageFooterParagraph.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at tellus at urna condimentum mattis. Ultrices vitae auctor eu augue. Vulputate eu scelerisque felis imperdiet proin fermentum. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Et magnis dis parturient montes nascetur ridiculus mus mauris.`;
+    homepageFooter.appendChild(homepageFooterParagraph);
+    const socialMediaLinksContainer = document.createElement("div");
+    socialMediaLinksContainer.class = "social-media__links";
+    const socialMediaLogos = [facebookLogo, instagramLogo, twitterLogo];
+    for (const images of socialMediaLogos) {
+      socialMediaLinksContainer.insertAdjacentElement("beforeend", images);
+    }
+
+    homepageFooter.insertAdjacentElement(
+      "beforeend",
+      socialMediaLinksContainer
+    );
+    const contactInfoSection = document.createElement("section");
+    const contactInfoSectionContent = [
+      "Phone: 405123456",
+      "Mobile: 405654321",
+      "Email: info@thisisjustatest.com",
+    ];
+    for (const info of contactInfoSectionContent) {
+      const contactInfoDiv = document.createElement("div");
+      contactInfoDiv.textContent = info;
+      contactInfoSection.appendChild(contactInfoDiv);
+    }
+    homepageFooter.appendChild(contactInfoSection);
+    return homepageFooter;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
 import Newpage from "./homepage";
-import "./style.css";
-import mcdBg from "./mcd-bg.jpg";
-import logo from "./mcd.jpg";
 
-function makeNewHeader() {
-  const newHeader = new Newpage().makeheader();
-  const mainLogo = new Image();
-  mainLogo.id = "main-logo";
-  mainLogo.src = logo;
-  mainLogo.alt = "business logo";
-  newHeader.insertAdjacentElement("afterbegin", mainLogo);
-  const menuButton = document.createElement("div");
-  menuButton.className = "menu-button";
-  for (let i = 0; i < 3; i++) {
-    const hamburgerLine = document.createElement("span");
-    menuButton.insertAdjacentElement("beforeend", hamburgerLine);
-  }
-  newHeader.insertAdjacentElement("beforeend", menuButton);
-  return newHeader;
-}
-
-function makeNewFooter() {
-  const newBody = new Newpage().makeFooter();
-    // const mainPicture = new Image();
-    // mainPicture.src = mcdBg;
-    // newBody.insertAdjacentElement("afterbegin", mainPicture);
-
-  return newBody;
-}
-
-document.body.appendChild(makeNewHeader());
+document.body.appendChild(new Newpage().makeheader());
 document.body.appendChild(new Newpage().makeMenuOptions());
 document.body.appendChild(new Newpage().makeRotatingPicturesSection());
 document.body.insertAdjacentElement("beforeend", (new Newpage().makeHomepageBody()));
-document.body.appendChild(makeNewFooter());
+document.body.appendChild(new Newpage().makeFooter());
